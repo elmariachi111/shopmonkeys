2b34fb7b79a76c8fbdac27947db59469108f791e
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2021-06-19 23:05:47 +0200

    gateway winston http logging

--- packages/gateway/index.ts
+++ packages/gateway/index.ts
@@ -5,13 +5,31 @@ import { JSONSchema7 } from 'json-schema'
 import RegisterSchema from './lib/RegisterSchema.json'
 const validator = new Validator({ allErrors: true })
 const validate = validator.validate
+import winston from 'winston'
+import expressWinston from 'express-winston'
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
 
-app.use('products', ProductRouter)
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      //winston.format.prettyPrint(),
+      winston.format.simple()
+      //winston.format.json()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: 'HTTP {{req.method}} {{req.url}}', // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+  })
+)
+
+//app.use('products', ProductRouter)
 
 app.post(
   '/register',
@@ -34,7 +52,7 @@ app.use(
   ) => {
     if (err.name === 'JsonSchemaValidationError') {
       res.status(400)
-      console.log(err)
+      console.error(err)
 
       return res.json({
         statusText: 'Bad Request',
