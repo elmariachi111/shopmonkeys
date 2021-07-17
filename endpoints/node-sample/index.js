7fc196e2c8ffa2f360ce96ac4fc87f8fc24d7c9a
Stefan <stadolf@gmail.com>
2021-07-17 12:14:17 +0200

    a ruby endpoint

--- endpoints/node-sample/index.js
+++ endpoints/node-sample/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
+app.all('/', (req, res) => {
   res.send('Hello World!')
 })
 
