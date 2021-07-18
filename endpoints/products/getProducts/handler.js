0e578f123b7bca281f98611432323c930e12aa5b
Stefan <stadolf@gmail.com>
2021-07-18 20:32:30 +0200

    getproducts: uses knex, considers query params

--- endpoints/products/getProducts/handler.js
+++ endpoints/products/getProducts/handler.js
@@ -2,16 +2,31 @@
 const fs = require("fs");
 const mysql = require('mysql2/promise');
 
-module.exports = async (event, context) => {
-
   //https://docs.openfaas.com/reference/secrets/
   const mysqlConnection = fs.readFileSync("/var/openfaas/secrets/mysql-connection", {
     encoding: "utf-8"
   });
 
-  const connection = await mysql.createConnection(mysqlConnection);
-  const [rows, fields] = await connection.execute("SELECT * FROM products;");
-  connection.end();
+
+
+module.exports = async (event, context) => {
+
+  const knex = require('knex')({
+    client: 'mysql2',
+    connection: mysqlConnection,
+    pool: { min: 1, max: 2 }
+  });
+
+  const query = knex.select().from("products").limit(20);
+  if (event.query) {
+    const { search } = event.query;
+    if (search) {
+      query.whereRaw("MATCH title AGAINST (?)", search)      
+    }
+  }
+  const rows = await query;
+  //const [rows, fields] = await connection.execute("SELECT * FROM products LIMIT 20;");
+  knex.destroy();
 
   return context
     .headers({
